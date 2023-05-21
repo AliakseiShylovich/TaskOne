7964c2fc61a566f4485c128caab470f26c4646ec
AliakseiShylovich <131357751+AliakseiShylovich@users.noreply.github.com>
2023-05-21 16:25:56 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int[] array = {1, 2, 1, 1, 2, -1};
-        int numberOff = 1;                          // хранит значение переменной которую необходимо исключить из суммы
+        int numberOff = 1;                          // хранит число которое необходимо исключить из суммы
         int sum = 0;                                // хранит значение суммы элементов мвссива
         for (int i = 0; i < array.length; i++) {
             if (array[i] == numberOff) {
