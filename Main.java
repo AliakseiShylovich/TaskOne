0898a79f9b7d60bedaa2edf691af1e59eece02ea
AliakseiShylovich <131357751+AliakseiShylovich@users.noreply.github.com>
2023-05-21 16:25:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 1, 1, 2, -1};
+        int numberOff = 1;                          // хранит значение переменной которую необходимо исключить из суммы
+        int sum = 0;                                // хранит значение суммы элементов мвссива
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == numberOff) {
+                continue;
+            }
+            sum = sum + array[i];
+        }
+        System.out.println(sum);
+    }
+}
